026b8e62a17e912e819e8b757634edf63e2e7fde
Akshat Kumar <akshatkumar.2001@gmail.com>
2023-09-09 00:51:58 +0530

    Minor Changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
-import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { ChakraProvider } from "@chakra-ui/react"
 import UserRegistration from './components/Registration'
 import UserLogin from './components/Login'
 import FormBuilder from './components/FormBuilder'
 import FormSubmission from './components/FormSubmission'
+import HomePage from './components/Home'
 
 function App() {
   return (
@@ -12,13 +12,16 @@ function App() {
       <Router>
         <div>
           <Routes>
+            {/* Home Page */}
+            <Route path="/" element={<HomePage />} />
+            
             {/* Public Routes */}
             <Route path="/register" element={<UserRegistration />} />
             <Route path="/login" element={<UserLogin />} />
             
             {/* Private Routes */}
             <Route path="/create" element={<FormBuilder />} />
-            <Route path="/submission/:id" element={<FormSubmission />} />
+            <Route path="/submission" element={<FormSubmission />} />
 
             {/* Add more routes as needed */}
           </Routes>
