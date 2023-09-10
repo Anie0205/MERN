1efef569c680d9a7160d2ab77a705c57fe4aa9d9
Akshat Kumar <akshatkumar.2001@gmail.com>
2023-09-11 02:39:13 +0530

    Minor Change

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,22 +1,80 @@
-// Login.js
+// // Login.js
+// import React, { useState } from 'react'
+// import { useNavigate } from "react-router-dom"
+
+// const UserLogin = () => {
+//     const [email, setEmail] = useState('')
+//     const [password, setPassword] = useState('')
+//     const navigate = useNavigate()
+
+//     const handleLogin = async () => {
+//         // Send login data to the backend
+//         try {
+//             const response = await fetch('/auth/login', {
+//                 method: 'POST',
+//                 headers: {
+//                     'Content-Type': 'application/json',
+//                 },
+//                 body: JSON.stringify({ email, password }),
+//             })
+//             if (response.ok) {
+//                 alert('Login successful')
+//                 navigate('/submission')
+//             }
+//         } catch (err) {
+//             console.log(err)
+//         }
+//     }
+
+//     return (
+//         <div>
+//             <input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+//             <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+//             <button onClick={handleLogin}>Login</button>
+//         </div>
+//     )
+// }
+
+// export default UserLogin
+
+import Avatar from '@mui/material/Avatar'
+import Button from '@mui/material/Button'
+import CssBaseline from '@mui/material/CssBaseline'
+import TextField from '@mui/material/TextField'
+import FormControlLabel from '@mui/material/FormControlLabel'
+import Checkbox from '@mui/material/Checkbox'
+import Link from '@mui/material/Link'
+import Grid from '@mui/material/Grid'
+import Box from '@mui/material/Box'
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined'
+import Typography from '@mui/material/Typography'
+import Container from '@mui/material/Container'
+import { createTheme, ThemeProvider } from '@mui/material/styles'
 import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+// TODO remove, this demo shouldn't need to reset the theme.
+
+const defaultTheme = createTheme()
 
 const UserLogin = () => {
-    const [email, setEmail] = useState('')
+    const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
+    const navigate = useNavigate()
 
-    const handleLogin = async () => {
-        // Send login data to the backend
+    const handleLogin = async (e) => {
+        e.preventDefault()
         try {
             const response = await fetch('/auth/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
                 },
-                body: JSON.stringify({ email, password }),
+                body: JSON.stringify({ username, password }),
             })
             if (response.ok) {
                 alert('Login successful')
+                navigate('/submission')
             }
         } catch (err) {
             console.log(err)
@@ -24,11 +82,72 @@ const UserLogin = () => {
     }
 
     return (
-        <div>
-            <input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
-            <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
-            <button onClick={handleLogin}>Login</button>
-        </div>
+        <ThemeProvider theme={defaultTheme}>
+            <Container component="main" maxWidth="xs">
+                <CssBaseline />
+                <Box
+                    sx={{
+                        marginTop: 8,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                    }}
+                >
+                    <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                        <LockOutlinedIcon />
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        Sign in
+                    </Typography>
+                    <Box component="form" noValidate sx={{ mt: 1 }}>
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="username"
+                            label="Username"
+                            name="username"
+                            onChange={(e) => setUsername(e.target.value)}
+                        />
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="password"
+                            label="Password"
+                            name="password"
+                            type="password"
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                        <FormControlLabel
+                            control={<Checkbox value="remember" color="primary" />}
+                            label="Remember me"
+                        />
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            onClick={handleLogin}
+                            sx={{ mt: 3, mb: 2 }}
+                        >
+                            Sign In
+                        </Button>
+                        <Grid container>
+                            <Grid item xs>
+                                <Link href="#" variant="body2">
+                                    Forgot password?
+                                </Link>
+                            </Grid>
+                            <Grid item>
+                                <Link href="#" variant="body2">
+                                    {"Don't have an account? Sign Up"}
+                                </Link>
+                            </Grid>
+                        </Grid>
+                    </Box>
+                </Box>
+            </Container>
+        </ThemeProvider>
     )
 }
 
--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,7 +1,6 @@
 const jwt = require('jsonwebtoken')
 const User = require('../models/User')
 const bcrypt = require('bcrypt')
-const sessionStorage = require('node-sessionstorage')
 
 const createToken = (id) => {
     return jwt.sign({ _id: id}, process.env.SECRET, { expiresIn: "3d" })
@@ -27,31 +26,32 @@ const registerUser = async (req, res) => {
         })
         const token = createToken(user._id)
 
-        sessionStorage.setItem('token', token)
-        sessionStorage.setItem('username', username)
-        res.status(200).json({ message: 'Registered successfully' })
+        localStorage.setItem('token', token)
+        res.status(200).json({ token })
     } catch (error) {
         return res.status(400).json({ message: error.message })
     }
 }
 
 const loginUser = async (req, res) => {
-    const { username, password } = req.body
+    try {
+        const { username, password } = req.body
     
-    const user = await User.findOne({ username })
-    if (!user) {
-        return res.status(400).json({ message: 'User not found' })
-    }
+        const user = await User.findOne({ username })
+        if (!user) {
+            return res.status(400).json({ message: 'User not found' })
+        }
 
-    const validPassword = await bcrypt.compare(password, user.password)
-    if (!validPassword) {
-        return res.status(400).json({ message: 'Invalid password' })
-    }
+        const validPassword = await bcrypt.compare(password, user.password)
+        if (!validPassword) {
+            return res.status(400).json({ message: 'Invalid password' })
+        }
 
-    const token = createToken(user._id)
-    sessionStorage.setItem('token', token)
-    sessionStorage.setItem('username', username)
-    res.status(200).json({ message: 'Login successful' })
+        const token = createToken(user._id)
+        res.status(200).json({ token })
+    } catch (error) {
+        return res.status(400).json({ message: error.message })
+    }
 }
 
 module.exports = { loginUser, registerUser }
